2974cde193d194025a4c72bbc789fe4f19c156db
TheNicestGuyEver <88489626+TheNicestGuyEver@users.noreply.github.com>
2021-12-09 07:32:34 +0200

    Add files via upload
    HTML test

--- musictheory.py
+++ musictheory.py
@@ -1,10 +1,10 @@
-
-
+from flask import Flask, render_template
 
 notes = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B','C','C#','D','D#','E','F','F#','G','G#','A','A#','B','C','C#','D','D#','E','F','F#'
-,'G','G#','A','A#','B','C','C#','D','D#','E','F','F#','G','G#','A','A#','B','C','C#','D','D#','E','F','F#']
+,'G','G#','A','A#','B','C','C#','D','D#','E','F','F#','G','G#','A','A#','B','C','C#','D','D#','E','F','F#','G','G#','A','A#','B','C','C#','D','D#','E','F','F#','G','G#']
 
-modes = ['ionian','dorian','phrygian','lydian','myxolydian','aeolian','locrian','ionian','dorian','phrygian','lydian','myxolydian','aeolian','locrian',"ionian","dorian"]
+modes = ['ionian','dorian','phrygian','lydian','myxolydian','aeolian','locrian','ionian','dorian','phrygian','lydian','myxolydian','aeolian','locrian',"ionian","dorian"
+,'ionian','dorian','phrygian','lydian','myxolydian','aeolian','locrian','ionian','dorian','phrygian','lydian','myxolydian','aeolian','locrian',"ionian","dorian"]
 
 sl_key = notes.index(input(str('input key:',)))
 sl_mode = modes.index(input(str('input mode:',)))
@@ -18,12 +18,15 @@ notes_range = notes[sl_key:x]
 modes_range = modes[sl_mode:y]
 print(notes_range[0:7])
 
-listo = [0,2,4,5,7,9,11,12]
+listo = [0,2,4,5,7,9,11,12,14,16]
+ 
+
 mode_formula = listo[sl_mode:y]
 print(mode_formula[0:7])
 
 new_range = notes_range[0+12-mode_formula[0]:30]
 print(new_range[0:7])
+
 
 i = 0
 k = 0
@@ -34,6 +37,16 @@ while i < len(mode_formula):
         if k not in mode_letters:
             mode_letters.append(new_range[mode_formula[k]])
             k = k + 1
-print(mode_letters[0:7])
+print(f'{modes[sl_mode]} in the key of {notes[sl_key]} is: {str(mode_letters[0:7])[1:-1]}')
 
+
+
+app = Flask(__name__)
+@app.route("/")
+@app.route("/home")
+def home():
+    return f'{modes[sl_mode]} in the key of {notes[sl_key]} is: {str(mode_letters[0:7])[1:-1]}'
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5001)
 
